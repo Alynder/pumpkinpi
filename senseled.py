b1cdcb9100766852be324e16ac85079bcc71f6ac
Jessica Cherry <jessrepka@gmail.com>
2021-10-12 21:31:59 -0400

    reset array, set up for possible flicker effect, kept notes on bluetooth sounds but nothing connecting to pi
    Signed-off-by: Jessica Cherry <jessrepka@gmail.com>

--- senseled.py
+++ senseled.py
@@ -2,27 +2,38 @@
 
 import RPi.GPIO as GPIO
 import os
+from time import sleep
 
 ledPin1 = 12 # define ledPins
-ledPin2 = 16
+ledPin2 = 22
 sensorPin = 11    # define sensorPin
+leds = [ledPin1, ledPin2]
 
 def setup():
     GPIO.setmode(GPIO.BOARD)        # use PHYSICAL GPIO Numbering
     GPIO.setup(ledPin1, GPIO.OUT)    # set ledPin to OUTPUT mode
     GPIO.setup(ledPin2, GPIO.OUT)    # set ledPin to OUTPUT mode
     GPIO.setup(sensorPin, GPIO.IN)  # set sensorPin to INPUT mode
+    GPIO.output(ledPin1, GPIO.LOW)
+    GPIO.output(ledPin2, GPIO.LOW)
+    
+ 
 
-def loop():
+
+def main():
     while True:
-        if GPIO.input(sensorPin)==GPIO.HIGH:
-            GPIO.output(ledPin1,GPIO.HIGH) # turn on led's
-            GPIO.output(ledPin2,GPIO.HIGH)
-            os.system("mplayer -ao alsa:device=bluetooth witch.wav")
-            print ('led turned on >>>')
-        else :
-            GPIO.output(ledPin,GPIO.LOW) # turn off led
-            print ('led turned off <<<')
+        for led in leds:
+            if GPIO.input(sensorPin)==GPIO.HIGH:
+                        GPIO.output(led, GPIO.HIGH)
+                        sleep(.25)
+                        GPIO.output(led, GPIO.LOW)
+                        print ('led turned on >>>')
+            else :
+                GPIO.output(led, GPIO.LOW) # turn off led
+                print ('led turned off <<<')
+             
+
+
 
 def destroy():
     GPIO.cleanup()                     # Release GPIO resource
@@ -31,7 +42,7 @@ if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
     setup()
     try:
-        loop()
+        main()
     except KeyboardInterrupt:  # Press ctrl-c to end the program.
         destroy()
 
